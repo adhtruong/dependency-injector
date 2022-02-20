283ab109a669fab16c9b0ee55d518485b339169e
Andrew Truong <adh.truong@gmail.com>
2022-02-20 13:02:40 +0000

    Add implementation

---
+++ di/core.py
@@ -0,0 +1,94 @@
+import inspect
+from contextlib import suppress
+from logging import getLogger
+from typing import Any, Callable, Generic, Iterator, TypeVar, Union, overload
+
+logger = getLogger(__name__)
+
+_RType = TypeVar("_RType")
+
+
+class _Depends(Generic[_RType]):
+    def __init__(
+        self,
+        dependency: Union[Callable[..., _RType], Callable[..., Iterator[_RType]]],
+        *,
+        use_cache: bool = True,
+    ):
+        self.dependency = dependency
+        self.use_cache = use_cache
+
+
+@overload
+def Depends(
+    dependency: Callable[..., Iterator[_RType]],
+    *,
+    use_cache: bool = True,
+) -> _RType:
+    ...
+
+
+@overload
+def Depends(
+    dependency: Callable[..., _RType],
+    *,
+    use_cache: bool = True,
+) -> _RType:
+    ...
+
+
+def Depends(
+    dependency: Union[Callable[..., _RType], Callable[..., Iterator[_RType]]],
+    *,
+    use_cache: bool = True,
+) -> _RType:
+    return _Depends(dependency, use_cache=use_cache)  # type: ignore
+
+
+class Resolver:
+    def __init__(self) -> None:
+        self._cache: dict[Callable, Any] = {}
+
+    def __call__(self, f: Callable[..., _RType]) -> _RType:
+        result, teardowns = _resolve(f, self._cache)
+        for teardown in reversed(teardowns):
+            with suppress(StopIteration):
+                next(teardown)
+
+        return result
+
+
+def resolve(f: Callable[..., _RType]) -> _RType:
+    resolver = Resolver()
+    return resolver(f)
+
+
+def _resolve(f: Callable[..., _RType], cache: dict[Callable, Any]) -> tuple[_RType, list[Iterator]]:
+    function_signature = inspect.signature(f)
+    resolved_parameters: dict[str, Any] = {}
+    teardowns: list[Iterator] = []
+    for parameter, value in function_signature.parameters.items():
+        default = value.default
+        if default is inspect.Parameter.empty:
+            raise RuntimeError(f"Unable to resolve {parameter = } for '{f.__name__}'")
+
+        if not isinstance(default, _Depends):
+            resolved_parameters[parameter] = default
+            continue
+
+        if default.use_cache and default.dependency in cache:
+            resolved_value = cache[default.dependency]
+        else:
+            resolved_value, teardowns_ = _resolve(default.dependency, cache)
+            teardowns.extend(teardowns_)
+            cache[default.dependency] = resolved_value
+        resolved_parameters[parameter] = resolved_value
+
+    result_generator = f(**resolved_parameters)
+    if isinstance(result_generator, Iterator):
+        resolved_value = next(result_generator)
+        teardowns.append(result_generator)
+    else:
+        resolved_value = result_generator
+
+    return resolved_value, teardowns
---
+++ test_core.py
@@ -0,0 +1,111 @@
+from typing import Iterator, List
+
+import pytest
+
+from di.core import Depends, resolve
+
+
+def test_core() -> None:
+    def get_a() -> int:
+        return 1
+
+    def get_b(a: int = Depends(get_a)) -> int:
+        return 1 + a
+
+    assert resolve(get_b) == 2
+
+
+def test_generator() -> None:
+    teardown = False
+
+    def my_generator() -> Iterator[int]:
+        yield 1
+        nonlocal teardown
+        teardown = True
+
+    def entry(a: int = Depends(my_generator)) -> int:
+        return a
+
+    assert resolve(entry) == 1
+    assert teardown
+
+
+def test_nested() -> None:
+    calls = 0
+
+    def my_function() -> int:
+        nonlocal calls
+        calls += 1
+        return 1
+
+    def my_other_function(a: int = Depends(my_function)) -> int:
+        return a
+
+    def entrypoint(a: int = Depends(my_function), b: int = Depends(my_other_function)) -> int:
+        return a + b
+
+    assert resolve(entrypoint) == 2
+    assert calls == 1
+
+
+def test_cache() -> None:
+    calls = 0
+
+    def my_function() -> int:
+        nonlocal calls
+        calls += 1
+        return 1
+
+    def entrypoint(a: int = Depends(my_function), b: int = Depends(my_function)) -> int:
+        return a + b
+
+    assert resolve(entrypoint) == 2
+    assert calls == 1
+
+
+def test_no_cache() -> None:
+    calls = 0
+
+    def my_function() -> int:
+        nonlocal calls
+        calls += 1
+        return 1
+
+    def entrypoint(
+        a: int = Depends(my_function), b: int = Depends(my_function, use_cache=False)
+    ) -> int:
+        return a + b
+
+    assert resolve(entrypoint) == 2
+    assert calls == 2
+
+
+def test_exception() -> None:
+    def entrypoint(unresolved: int) -> None:
+        return None
+
+    with pytest.raises(
+        RuntimeError, match="Unable to resolve parameter = 'unresolved' for 'entrypoint'"
+    ):
+        resolve(entrypoint)
+
+
+def test_default_value() -> None:
+    def my_function(parameter: int = 1) -> int:
+        return parameter
+
+    def entrypoint(parameter: int = Depends(my_function)) -> int:
+        return parameter
+
+    assert resolve(my_function) == 1
+    assert resolve(entrypoint) == 1
+
+
+def test_list_return() -> None:
+    def my_function() -> List[int]:
+        return [1, 2]
+
+    def entry(param: list[int] = Depends(my_function)) -> list[int]:
+        return param
+
+    assert resolve(entry) == [1, 2]
